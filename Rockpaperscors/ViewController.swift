aea46a73651f2c3942d781f20aef6473e0e4e129
Luis Segovia <luiissegovia@icloud.com>
2022-05-10 13:31:43 -0600

    mvp

--- Rockpaperscors/ViewController.swift
+++ Rockpaperscors/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var computerImage: UIImageView!
+    
+    @IBOutlet weak var yourselection: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockbutt(_ sender: Any) {
+    }
+    
+    @IBAction func papery(_ sender: Any) {
+    }
+    
+    @IBAction func scissorssss(_ sender: Any) {
+    }
+    
+    
 }
 
